82d9b16d9a69ad0cc97fd30cc771138caf3d2017
Adebisi <adecoded001@gmail.com>
2022-12-13 03:27:42 -0800

    working on lower alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,10 @@
 #include "main.h"
-
 /**
- * main- print_alphabet - prints the alphabe
- * Descriptions: print alphabet in lowercase
+ * main - check the code
+ * Description: print alphabet in lowercase
+ *
+ * Return: Always 0
  */
-
 void print_alphabet(void)
 {
 	char alpha;
